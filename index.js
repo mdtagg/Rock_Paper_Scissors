a5f02a1fb2bdbd95e615e0bea83ddc6354e57aaf
Mike Tagg <mdtagg91@gmail.com>
2022-09-11 16:48:07 -0700

    game function added
    game function was created to run playRound a number of times and keep track of the results

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ function getComputerChoice() {
 
 getComputerChoice()
 
-function playRound(playerSelection) {
-    playerSelection.toLowerCase()
-    console.log(playerSelection)
-    console.log(computerSelection)
+function playRound(playerSelection,computerSelection) {
 
     if(playerSelection === 'rock' && computerSelection === 'paper'
     || playerSelection === 'paper' && computerSelection === 'scissors' 
@@ -23,13 +20,56 @@ function playRound(playerSelection) {
     || playerSelection === 'rock' && computerSelection === 'scissors') {
         return `You win! ${playerSelection} beats ${computerSelection}`
     }
-    
     else{
         return 'Its a draw! Play again'
-        
     }
     
 }
+
 let playerSelection = 'rock'
 let computerSelection = getComputerChoice()
 playRound(playerSelection,computerSelection)
+
+
+function game() {
+    let gameResults = [];
+    
+    for(let i = 0; i < 5; i++) {
+        let computerSelection = getComputerChoice();
+        let playerSelection = prompt().toLowerCase()
+        gameResults.push(playRound(playerSelection,computerSelection))
+    }
+    console.log(gameResults)
+    checkGameResults(gameResults)
+}
+
+game()
+
+function checkGameResults(results) {
+    let wins = 0;
+    let loses = 0;
+    let draws = 0;
+
+    for(let i = 0; i < results.length;i++) {
+        if(results[i].includes('win')) {
+            wins++
+        }
+        else if(results[i].includes('lose')) {
+            loses++
+        }
+        else {
+            draws++
+        }
+    }
+    
+    if(wins > loses) {
+        console.log(`You won! You won ${wins} out of ${wins + loses + draws} times`)
+    }
+    else if(loses > wins) {
+        console.log(`You lost! You lost ${loses} out of ${wins + loses + draws} times`)
+    }
+    else {
+        console.log(`Its a draw! Please play again`)
+    }
+}
+
