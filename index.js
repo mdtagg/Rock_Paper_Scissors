09a7af8b656dbd235c5e61f21d10335460c04d82
Mike Tagg <mdtagg91@gmail.com>
2022-09-11 15:29:52 -0700

    add playRound function
    Play round makes the player selection case insensitive with .toLowerCase()
    
    computer selection is set to getComputerChoice as default
    
    Logic implemented to compare playerSelection with computerSelection

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 function getComputerChoice() {
     const rockPaperScissors = ['rock', 'paper', 'scissors']
     let randNum = Math.floor(Math.random() * rockPaperScissors.length)
-    console.log(rockPaperScissors[randNum])
     return rockPaperScissors[randNum]
 }
 
 getComputerChoice()
+
+function playRound(playerSelection,computerSelection = getComputerChoice()) {
+    playerSelection.toLowerCase()
+    console.log(playerSelection)
+    console.log(computerSelection)
+
+    if(playerSelection === 'rock' && computerSelection === 'paper'
+    || playerSelection === 'paper' && computerSelection === 'scissors' 
+    || playerSelection === 'scissors' && computerSelection === 'rock')
+    {
+        console.log( `You lose! ${computerSelection} beats ${playerSelection}`)
+    }
+    
+    else if(playerSelection === 'paper' && computerSelection === 'rock'
+    || playerSelection === 'scissors' && computerSelection === 'paper'
+    || playerSelection === 'rock' && computerSelection === 'scissors') {
+        console.log(`You win! ${playerSelection} beats ${computerSelection}`)
+    }
+    
+    else{
+        console.log('Its a draw! Play again')
+    }
+    
+}
+
+playRound('rock')
